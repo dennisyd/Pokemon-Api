1b397f114a2dd978dbd25ecd1010ec355f534139
dennisyd <dennisyd@alum.mit.edu>
2020-08-07 00:19:32 -0400

    creating db interface classes

---
+++ Pokemon_API/DataBaseInterface/DatabaseConnector.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Pokemon_API.DataBaseInterface
+{
+    internal class DatabaseConnector
+    {
+        //db connection here
+    }
+}
---
+++ Pokemon_API/DataBaseInterface/PokemonContructor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Pokemon_API.DataBaseInterface
+{
+    public class PokemonContructor
+    {
+        public PokemonContructor()
+        {
+            //get all the fields associated with given pokemon
+        }
+    }
+}
