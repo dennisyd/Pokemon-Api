8495c80d3a9f03626f9ebdf79b3c068e8d633432
dennisyd <dennisyd@alum.mit.edu>
2020-09-24 01:03:57 -0400

    Pokemon Abilities table

--- Pokemon_API/DatabaseSchemas/Pokemon/Models/Abilities.cs
+++ Pokemon_API/DatabaseSchemas/Pokemon/Models/Abilities.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 using Newtonsoft.Json;
 
@@ -7,7 +7,7 @@ namespace Pokemon_API.DatabaseSchemas.Pokemon.Models
     public class Abilities
     {
         [JsonProperty(PropertyName="id", Order = 1)]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [JsonProperty(PropertyName="pokemonNumber", Order = 2)]
         public int PokemonNumber { get; set; }
@@ -17,6 +17,42 @@ namespace Pokemon_API.DatabaseSchemas.Pokemon.Models
 
         public Abilities()
         {
+        }
+
+        public Abilities(int? Id, int PokemonNumber, string Ability)
+        {
+            this.Id = Id;
+            this.PokemonNumber = PokemonNumber;
+            this.Ability = Ability;
+        }
+
+        public Dictionary<string, object> ToDict()
+        {
+            return new Dictionary<string, object>()
+            {
+                {nameof(this.Id).ToLower(), this.Id },
+                {nameof(this.PokemonNumber).ToLower(), this.PokemonNumber },
+                {nameof(this.Ability).ToLower(), this.Ability }
+            };
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Abilities;
+            if (item == null)
+            {
+                return false;
+            }
+
+            return
+                item.Id.Equals(this.Id) &&
+                item.PokemonNumber.Equals(this.PokemonNumber) &&
+                item.Ability.Equals(this.Ability);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetHashCode();
         }
     }
 }
--- Pokemon_API/DatabaseSchemas/Pokemon/Tables/Abilities.cs
+++ Pokemon_API/DatabaseSchemas/Pokemon/Tables/Abilities.cs
@@ -11,31 +11,55 @@ namespace Pokemon_API.DatabaseSchemas.Pokemon.Tables
     public class Abilities : TableQueries<Models.Abilities>
     {
         public override string Database => "Pokemon";
-        public override string TableName => "Flags";
+        public override string TableName => "Abilities";
 
         public Abilities() { }
 
-        public async Task<List<Models.Abilities>> GetAll(int number)
+        public override DatabaseConnector GetDatabaseConnector()
         {
-
-            return null;
+            connection.SetDatabase(Database);
+            return base.GetDatabaseConnector();
         }
 
-        public async Task<Models.Abilities> Get(int number)
+        public async Task<List<Models.Abilities>> Get(int number)
         {
+            Dictionary<string, object> dict = new Dictionary<string, object>()
+            {
+                {"pokemonNumber", number }
+            };
 
-            return null;
+            List<Models.Abilities> result = await Get(dict);
+            return result;
         }
 
-        public async Task<Models.Abilities> Insert(Models.Abilities obj)
+        public async Task<int?> Insert(Models.Abilities obj)
         {
-
-            return null;
+            int? result = await Insert(obj.ToDict());
+            return result;
         }
 
-        public async Task Delete(int number)
+        public async Task<int?> Delete(int number)
         {
+            Dictionary<string, object> dict = new Dictionary<string, object>()
+            {
+                {"pokemonNumber", number }
+            };
 
+            int? result = await Delete(dict);
+            return result;
+        }
+
+        public override Models.Abilities DataReaderConverter(MySqlDataReader reader)
+        {
+            int id = int.Parse(reader["id"].ToString());
+            int pokemonNumber = int.Parse(reader["pokemonNumber"].ToString());
+            string ability = reader["ability"].ToString();
+
+            return new Models.Abilities(
+                Id: id,
+                PokemonNumber: pokemonNumber,
+                Ability: ability
+            );
         }
     }
 }
