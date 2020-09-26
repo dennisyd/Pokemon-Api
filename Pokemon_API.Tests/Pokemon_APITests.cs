659ab637962a678204232b49d2b0c6ecf65c3ec6
dennisyd <dennisyd@alum.mit.edu>
2020-09-26 00:45:48 -0400

    Update Pokemon_APITests.cs
    test coverage for remaining tables passing

--- Pokemon_API.Tests/Pokemon_APITests.cs
+++ Pokemon_API.Tests/Pokemon_APITests.cs
@@ -354,14 +354,14 @@ namespace Pokemon_API.Tests
         }
 
         [Fact]
-        public async void Test_Pokemon_EggGroups()
+        public async void Test_Pokemon_Evolutions()
         {
-            Console.WriteLine("-- | Pokemon | EggGroups | --");
+            Console.WriteLine("-- | Pokemon | Evolutions | --");
             Console.WriteLine("-- Testing Connection --");
             DatabaseConnector connection;
             bool isConnected;
 
-            var table = new DatabaseSchemas.Pokemon.Tables.EggGroups();
+            var table = new DatabaseSchemas.Pokemon.Tables.Evolutions();
             connection = table.GetDatabaseConnector();
             isConnected = await connection.IsConnected();
             Assert.True(isConnected);
@@ -370,10 +370,10 @@ namespace Pokemon_API.Tests
             Console.WriteLine(SUCCESS);
             Console.WriteLine("-- Testing Insert --");
 
-            var data = new DatabaseSchemas.Pokemon.Models.EggGroups(
+            var data = new DatabaseSchemas.Pokemon.Models.Evolutions(
                 Id: null,
                 PokemonNumber: -1,
-                EggGroup: "eggGroup"
+                Evolution: "evolution"
             );
 
             await table.Delete(data.PokemonNumber);
@@ -384,7 +384,7 @@ namespace Pokemon_API.Tests
             Console.WriteLine(SUCCESS);
             Console.WriteLine("-- Testing Get --");
 
-            List<DatabaseSchemas.Pokemon.Models.EggGroups> list = await table.Get(data.PokemonNumber);
+            List<DatabaseSchemas.Pokemon.Models.Evolutions> list = await table.Get(data.PokemonNumber);
             var get = list.FirstOrDefault();
 
             Assert.NotNull(get);
@@ -401,6 +401,193 @@ namespace Pokemon_API.Tests
 
             list = await table.Get(data.PokemonNumber);
             Assert.Empty(list);
+
+            Console.WriteLine(SUCCESS);
+        }
+
+        [Fact]
+        public async void Test_Pokemon_GenderRatio()
+        {
+            Console.WriteLine("-- | Pokemon | GenderRatio | --");
+            Console.WriteLine("-- Testing Connection --");
+            DatabaseConnector connection;
+            bool isConnected;
+
+            var table = new DatabaseSchemas.Pokemon.Tables.GenderRatio();
+            connection = table.GetDatabaseConnector();
+            isConnected = await connection.IsConnected();
+            Assert.True(isConnected);
+            await connection.Disconnect();
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Insert --");
+
+            var data = new DatabaseSchemas.Pokemon.Models.GenderRatio(
+                Id: null,
+                PokemonNumber: -1,
+                Male: 1f,
+                Female: 2f
+            );
+
+            await table.Delete(data.PokemonNumber);
+
+            int? result = await table.Insert(data);
+            Assert.NotNull(result);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Get --");
+
+            DatabaseSchemas.Pokemon.Models.GenderRatio get = await table.Get(data.PokemonNumber);
+
+            Assert.NotNull(get);
+            Assert.NotNull(get.Id);
+
+            data.Id = get.Id;
+            Assert.Equal(data, get);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Delete --");
+
+            result = await table.Delete(data.PokemonNumber);
+            Assert.NotNull(result);
+
+            get = await table.Get(data.PokemonNumber);
+            Assert.Null(get);
+
+            Console.WriteLine(SUCCESS);
+        }
+
+        [Fact]
+        public async void Test_Pokemon_Moves()
+        {
+            Console.WriteLine("-- | Pokemon | Moves | --");
+            Console.WriteLine("-- Testing Connection --");
+            DatabaseConnector connection;
+            bool isConnected;
+
+            var table = new DatabaseSchemas.Pokemon.Tables.Moves();
+            connection = table.GetDatabaseConnector();
+            isConnected = await connection.IsConnected();
+            Assert.True(isConnected);
+            await connection.Disconnect();
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Insert --");
+
+            var data = new DatabaseSchemas.Pokemon.Models.Moves(
+                Id: null,
+                PokemonNumber: -1,
+                MoveNumber: -2,
+                Level: -3,
+                IsTM: false,
+                IsHM: false
+            );
+
+            await table.Delete(data.PokemonNumber);
+
+            int? result = await table.Insert(data);
+            Assert.NotNull(result);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Get --");
+
+            List<DatabaseSchemas.Pokemon.Models.Moves> list = await table.Get(data.PokemonNumber);
+            var get = list.FirstOrDefault();
+
+            Assert.NotNull(get);
+            Assert.NotNull(get.Id);
+
+            data.Id = get.Id;
+            Assert.Equal(data, get);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Delete --");
+
+            result = await table.Delete(data.PokemonNumber);
+            Assert.NotNull(result);
+
+            list = await table.Get(data.PokemonNumber);
+            Assert.Empty(list);
+
+            Console.WriteLine(SUCCESS);
+        }
+
+        [Fact]
+        public async void Test_Pokemon_Pokemon()
+        {
+            Console.WriteLine("-- | Pokemon | Pokemon | --");
+            Console.WriteLine("-- Testing Connection --");
+            DatabaseConnector connection;
+            bool isConnected;
+
+            var table = new DatabaseSchemas.Pokemon.Tables.Pokemon();
+            connection = table.GetDatabaseConnector();
+            isConnected = await connection.IsConnected();
+            Assert.True(isConnected);
+            await connection.Disconnect();
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Insert --");
+
+            var data = new DatabaseSchemas.Pokemon.Models.Pokemon(
+                Id: null,
+                Name: "test",
+                Number: -1,
+                Species: "species",
+                Height: -2f,
+                Weight: -3f,
+                Color: "color",
+                EvolutionLevel: null
+            );
+
+            await table.Delete(data.Number);
+
+            int? result = await table.Insert(data);
+            Assert.NotNull(result);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Get Name --");
+
+            DatabaseSchemas.Pokemon.Models.Pokemon getName = await table.Get(data.Name);
+
+            Assert.NotNull(getName);
+            Assert.NotNull(getName.Id);
+
+            data.Id = getName.Id;
+            Assert.Equal(data, getName);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Delete Name--");
+
+            result = await table.Delete(data.Name);
+            Assert.NotNull(result);
+
+            getName = await table.Get(data.Name);
+            Assert.Null(getName);
+
+            Console.WriteLine("-- Testing Insert --");
+            result = await table.Insert(data);
+            Assert.NotNull(result);
+
+            Console.WriteLine(SUCCESS);
+
+            Console.WriteLine("-- Testing Get Number --");
+            DatabaseSchemas.Pokemon.Models.Pokemon getNumber = await table.Get(data.Number);
+
+            Assert.NotNull(getNumber);
+            Assert.NotNull(getNumber.Id);
+
+            data.Id = getNumber.Id;
+            Assert.Equal(data, getNumber);
+
+            Console.WriteLine(SUCCESS);
+            Console.WriteLine("-- Testing Delete Number --");
+
+            result = await table.Delete(data.Number);
+            Assert.NotNull(result);
+
+            getName = await table.Get(data.Number);
+            Assert.Null(getName);
 
             Console.WriteLine(SUCCESS);
         }
