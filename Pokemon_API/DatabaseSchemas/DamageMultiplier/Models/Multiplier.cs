155b6d3ccb556f61e140861b899be49fc89956a6
dennisyd <dennisyd@alum.mit.edu>
2020-10-29 22:53:35 -0400

    removing unneccessary json properties

--- Pokemon_API/DatabaseSchemas/DamageMultiplier/Models/Multiplier.cs
+++ Pokemon_API/DatabaseSchemas/DamageMultiplier/Models/Multiplier.cs
@@ -6,64 +6,44 @@ namespace Pokemon_API.DatabaseSchemas.DamageMultiplier.Models
 {
     public class Multiplier : DatabaseTable<Multiplier>
     {
-        [JsonIgnore]
         public int? Id{ get; set; }
 
-        [JsonProperty(PropertyName="name", Order = 1)]
         public string Name { get; set; }
 
-        [JsonProperty(PropertyName="normal", Order = 2)]
         public float Normal{ get; set; }
 
-        [JsonProperty(PropertyName="fighting", Order = 3)]
         public float Fighting{ get; set; }
 
-        [JsonProperty(PropertyName="flying", Order = 4)]
         public float Flying{ get; set; }
 
-        [JsonProperty(PropertyName="poison", Order = 5)]
         public float Poison{ get; set; }
 
-        [JsonProperty(PropertyName="ground", Order = 6)]
         public float Ground{ get; set; }
 
-        [JsonProperty(PropertyName="rock", Order = 7)]
         public float Rock{ get; set; }
 
-        [JsonProperty(PropertyName="bug", Order = 8)]
         public float Bug{ get; set; }
 
-        [JsonProperty(PropertyName="ghost", Order = 9)]
         public float Ghost{ get; set; }
 
-        [JsonProperty(PropertyName="steel", Order = 10)]
         public float Steel{ get; set; }
 
-        [JsonProperty(PropertyName="fire", Order = 11)]
         public float Fire{ get; set; }
 
-        [JsonProperty(PropertyName="water", Order = 12)]
         public float Water{ get; set; }
 
-        [JsonProperty(PropertyName="grass", Order = 13)]
         public float Grass{ get; set; }
 
-        [JsonProperty(PropertyName="electric", Order = 14)]
         public float Electric{ get; set; }
 
-        [JsonProperty(PropertyName="psychic", Order = 15)]
         public float Psychic{ get; set; }
 
-        [JsonProperty(PropertyName="ice", Order = 16)]
         public float Ice{ get; set; }
 
-        [JsonProperty(PropertyName="dragon", Order = 17)]
         public float Dragon{ get; set; }
 
-        [JsonProperty(PropertyName="dark", Order = 18)]
         public float Dark{ get; set; }
 
-        [JsonProperty(PropertyName="fairy", Order = 19)]
         public float Fairy{ get; set; }
 
         public Multiplier()
--- Pokemon_API/DatabaseSchemas/Moves/Models/Flags.cs
+++ Pokemon_API/DatabaseSchemas/Moves/Models/Flags.cs
@@ -6,13 +6,10 @@ namespace Pokemon_API.DatabaseSchemas.Moves.Models
 {
     public class Flags : DatabaseTable<Flags>
     {
-        [JsonIgnore]
         public int? Id { get; set; }
 
-        [JsonIgnore]
         public int MoveNumber { get; set; }
 
-        [JsonProperty(PropertyName="flag", Order = 1)]
         public string Flag { get; set; }
 
         public Flags()
